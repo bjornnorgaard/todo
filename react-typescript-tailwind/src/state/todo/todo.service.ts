421fae1754ca9f1e6802d69bd1ab3ecc3765a9cf
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-11-21 16:53:13 +0100

    Added mock method for getting test data

--- react-typescript-tailwind/src/state/todo/todo.service.ts
+++ react-typescript-tailwind/src/state/todo/todo.service.ts
@@ -1,7 +1,13 @@
-import { todoStore } from "./todo.store";
+import { Todos, todoStore } from "./todo.store";
 
 export class todoService {
+
     static addTodo = (desc: string) => {
+        if (desc === "oij") {
+            todoService.addTestData();
+            return;
+        }
+
         const uuid = require("uuid");
         const id = uuid.v4();
         todoStore.push({id: id, description: desc, completed: false,})
@@ -15,5 +21,17 @@ export class todoService {
     static deleteTodo = (id: string) => {
         todoStore.splice(todoStore.findIndex(t => t.id === id), 1);
     };
+
+    private static addTestData() {
+        const initialState: Todos[] = [
+            {id: "1", description: "Walk the dog", completed: false},
+            {id: "2", description: "Do the dishes", completed: false},
+            {id: "3", description: "Code a project", completed: true},
+            {id: "4", description: "Cleanup living room", completed: false},
+            {id: "5", description: "Move the furniture", completed: true},
+            {id: "6", description: "Something fun", completed: false},
+        ];
+        initialState.forEach(t => todoStore.push(t));
+    }
 }
 
--- react-typescript-tailwind/src/state/todo/todo.store.ts
+++ react-typescript-tailwind/src/state/todo/todo.store.ts
@@ -6,13 +6,4 @@ export interface Todos {
     description: string;
 }
 
-const initialState: Todos[] = [
-    {id: "1", description: "Walk the dog", completed: false},
-    {id: "2", description: "Do the dishes", completed: false},
-    {id: "3", description: "Code a project", completed: true},
-    {id: "4", description: "Cleanup living room", completed: false},
-    {id: "5", description: "Move the furniture", completed: true},
-    {id: "6", description: "Something fun", completed: false},
-];
-
-export const todoStore = proxy<Todos[]>(initialState);
+export const todoStore = proxy<Todos[]>([]);
