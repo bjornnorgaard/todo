88d9a6ac6cceb4ecc2c726a19f05716d5eef0935
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-11-21 16:18:58 +0100

    Moved nav to own file and added mb

--- react-typescript-tailwind/src/App.tsx
+++ react-typescript-tailwind/src/App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
+import { Navigation } from "./components/Navigation";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
-
-export const Navigation = () => {
-    const routes = [
-        {route: "/", label: "Home"},
-        {route: "/about", label: "About"},
-    ];
-
-    return (
-        <nav className="mb-4 w-full bg-red-200 p-2 drop-shadow">
-            <ul className="flex justify-between px-8 text-3xl font-bold">
-                {routes.map(r => (
-                    <li className="drop-shadow hover:text-blue-400 hover:underline">
-                        <Link
-                            key={r.route}
-                            to={r.route}>
-                            {r.label}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
-};
 
 export const App = () => (
     <div className="m-auto mt-8 flex max-w-xs flex-col items-center">
---
+++ react-typescript-tailwind/src/components/Navigation.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Navigation = () => {
+    const routes = [
+        {route: "/", label: "Home"},
+        {route: "/about", label: "About"},
+    ];
+
+    return (
+        <nav className="mb-16 w-full bg-red-200 p-2 drop-shadow">
+            <ul className="flex justify-between px-8 text-3xl font-bold">
+                {routes.map(r => (
+                    <li className="drop-shadow hover:text-blue-400 hover:underline">
+                        <Link
+                            key={r.route}
+                            to={r.route}>
+                            {r.label}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
