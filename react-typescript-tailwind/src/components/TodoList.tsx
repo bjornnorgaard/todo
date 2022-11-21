d2aad236576fd806790832ad50984a81a2e5f94f
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2022-11-21 16:17:28 +0100

    Setup imported font for hand written look

--- react-typescript-tailwind/src/components/TodoList.tsx
+++ react-typescript-tailwind/src/components/TodoList.tsx
@@ -8,7 +8,7 @@ export const TodoList = () => {
 
     return (
         <div className="mt-4 w-full bg-amber-300 p-4 drop-shadow-lg bg-paper-pattern">
-            <h2 className="mb-4 text-center text-2xl underline">Things to do</h2>
+            <h2 className="mb-4 text-center text-2xl underline font-hand text-4xl">Things to do</h2>
             {todos.map(t =>
                 <div key={t.id}
                      className="flex flex-row justify-between gap-4 py-1">
@@ -18,7 +18,7 @@ export const TodoList = () => {
                             : <FaRegCircle className="h-auto hover:text-blue-400"/>}
                     </button>
 
-                    <div className={t.completed ? "line-through text-left w-full" : "text-left w-full"}>
+                    <div className={t.completed ? "line-through text-left w-full text-2xl font-hand" : "text-2xl font-hand text-left w-full"}>
                         {t.description}
                     </div>
 
